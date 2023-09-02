7360f2eed97cdd6ce30fadee2d827c5f7eb2a3ab
unknown <wardahnabilah33@gmail.com>
2023-09-02 10:23:26 +0700

    Add initial files and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        'header-dark': '#050F11',
+        'primary-dark': '#02131B',
+        'primary-light': '#39989B',
+        'primary-red':'#D02D2D',
+        'primary-red-dark': '#541212',
+        'accent-yellow': '#F0DE36',
+        'accent-darkgrey': '#3E4646',
+        'accent-grey': '#69828C',
+        'accent-lightpurple': '#9BABB8',
+        'accent-darkerpurple': '#556877'
+      },
+    },
+  },
+  plugins: [],
+}
+
