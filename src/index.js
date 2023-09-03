54188e67cf06a85ec180b57ef7efe036587dcfd6
unknown <wardahnabilah33@gmail.com>
2023-09-03 08:33:41 +0700

    Add material-tailwind

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 import './index.css'
+import { ThemeProvider } from '@material-tailwind/react';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <Provider store={store}>
             <Router>
-                <App />
+                <ThemeProvider>
+                    <App />
+                </ThemeProvider>
             </Router>
         </Provider>
     </React.StrictMode>
