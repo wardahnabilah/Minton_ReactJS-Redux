73a51964914b483f350a1475407a2fc0a4d01748
unknown <wardahnabilah33@gmail.com>
2023-09-03 11:26:52 +0700

    Create CreateBooking page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Header } from "./components/layouts/Header/Header"
 import { Footer } from "./components/layouts/Footer/Footer"
 import { Routes, Route } from "react-router-dom"
 import { Homepage } from "./pages/Home/Homepage"
+import { CreateBooking } from "./pages/CreateBooking/CreateBooking"
 import { BookingSchedule } from "./pages/BookingSchedule/BookingSchedule"
 
 export function App() {
@@ -11,6 +12,7 @@ export function App() {
             <main className="relative min-h-screen dark:bg-primary-dark text-primary-dark dark:text-white">
                 <Routes>
                     <Route path="/" element={<Homepage />} />
+                    <Route path="/create-booking" element={<CreateBooking />} />
                     <Route path="/booking-schedule" element={<BookingSchedule />} />
                 </Routes>
             </main>
---
+++ src/pages/CreateBooking/CreateBooking.jsx
@@ -0,0 +1,7 @@
+export function CreateBooking() {
+    return (
+        <section className="py-28">
+            <h1 className="text-2.5xl text-center font-extrabold uppercase tracking-wider">Booking Form</h1>
+        </section>       
+    )
+}
