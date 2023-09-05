ba8fb78a4d1bf57c750219db9b4f71bf44f25880
unknown <wardahnabilah33@gmail.com>
2023-09-05 23:40:27 +0700

    Add styles for disabled ButtonBtn

--- src/components/elements/Buttons.jsx
+++ src/components/elements/Buttons.jsx
@@ -12,10 +12,27 @@ export function ButtonLink({pathName, children: label}) {
     )
 }
 
-export function ButtonBtn({onClick, className: addedClass,children: label}) {
+export function ButtonBtn({onClick, className: addedClass,children: label, disabled}) {
     return (
         <button 
-            className={`${addedClass} inline-block px-8 py-2 text-lg text-center rounded-full bg-primary-red shadow-lg hover:bg-primary-red-dark shadow-primary-red/40 text-white`}
+            className={`
+                ${addedClass} 
+                inline-block 
+                px-8 py-2 
+                text-lg 
+                text-center
+                text-white 
+                rounded-full 
+                bg-primary-red  
+                hover:bg-primary-red-dark
+                shadow-lg 
+                shadow-primary-red/40
+                disabled:shadow-none
+                disabled:text-white/40
+                disabled:bg-accent-darkgrey/70
+                dark:disabled:bg-accent-lightgrey/40
+            `}
+            disabled={disabled}
             onClick={onClick}
         >
             {label}
