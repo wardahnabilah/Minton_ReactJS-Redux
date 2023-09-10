2355850f06d4ce30f806dd63a196cea0f38a4f0d
unknown <wardahnabilah33@gmail.com>
2023-09-10 10:46:18 +0700

    Create bookingSchedule slice

---
+++ src/store/bookingScheduleSlice.js
@@ -0,0 +1,95 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const bookingScheduleSlice = createSlice({
+    name: 'bookingSchedule',
+    initialState: {
+        scheduleList: [
+            {
+                date: '10 September 2023',
+                hourList: [
+                    {
+                        hour: '08:00 - 09:00',
+                        isBooked: false
+                    },
+                    {
+                        hour: '16:00 - 17:00',
+                        isBooked: false
+                    },
+                    {
+                        hour: '18:00 - 19:00',
+                        isBooked: false
+                    }
+                ]
+            },
+            {
+                date: '11 September 2023',
+                hourList: [
+                    {
+                        hour: '08:00 - 09:00',
+                        isBooked: true
+                    },
+                    {
+                        hour: '16:00 - 17:00',
+                        isBooked: false
+                    },
+                    {
+                        hour: '18:00 - 19:00',
+                        isBooked: false
+                    }
+                ]
+            },
+            {
+                date: '12 September 2023',
+                hourList: [
+                    {
+                        hour: '08:00 - 09:00',
+                        isBooked: true
+                    },
+                    {
+                        hour: '16:00 - 17:00',
+                        isBooked: true
+                    },
+                    {
+                        hour: '18:00 - 19:00',
+                        isBooked: false
+                    }
+                ]
+            },
+            {
+                date: '13 September 2023',
+                hourList: [
+                    {
+                        hour: '08:00 - 09:00',
+                        isBooked: false
+                    },
+                    {
+                        hour: '16:00 - 17:00',
+                        isBooked: false
+                    },
+                    {
+                        hour: '18:00 - 19:00',
+                        isBooked: false
+                    }
+                ]
+            },
+        ]
+    },
+    reducers: {
+        updateSchedule: (state, action) => {
+            const { bookingDate, bookingHour} = action.payload
+
+            state.scheduleList.forEach(schedule => {
+                if(schedule.date === bookingDate) {
+                    schedule.hourList.forEach(hourListItem => {
+                        if(hourListItem.hour === bookingHour) {
+                            hourListItem.isBooked = true
+                        }
+                    })
+                }
+            })
+        }
+    }
+})
+
+export const { updateSchedule } = bookingScheduleSlice.actions
+export const bookingScheduleReducer = bookingScheduleSlice.reducer
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { themeReducer } from "./themeSlice";
 import { newBookingReducer } from "./newBookingSlice";
+import { bookingScheduleReducer } from "./bookingScheduleSlice";
 
 export const store = configureStore({
     reducer: {
         theme: themeReducer,
-        newBooking: newBookingReducer
+        newBooking: newBookingReducer,
+        bookingSchedule: bookingScheduleReducer
     }
 })
