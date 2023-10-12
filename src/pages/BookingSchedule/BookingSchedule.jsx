11ba997acede6de87772dbc46da90765fe743ee9
unknown <wardahnabilah33@gmail.com>
2023-10-12 22:08:23 +0700

    Fix booking schedules fetching

--- src/pages/BookingSchedule/BookingSchedule.jsx
+++ src/pages/BookingSchedule/BookingSchedule.jsx
@@ -5,9 +5,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchSchedules } from "../../store/bookingScheduleSlice";
 
 export function BookingSchedule() {
-    const [selectedDate, setSelectedDate] = useState('') 
-    const [dateOptions, setDateOptions] = useState([])
-    const { scheduleList, loading } = useSelector(state=>state.bookingSchedule)
+    const [selectedDate, setSelectedDate] = useState('')
+    const { scheduleList } = useSelector(state=>state.bookingSchedule)
     const dispatch = useDispatch()
     
     useDocTitle('Booking Schedule')
@@ -16,15 +15,6 @@ export function BookingSchedule() {
     useEffect(()=>{
         dispatch(fetchSchedules())
     }, [dispatch])
-
-    // Get the date options from bookingSchedule slice
-    useEffect(()=>{
-        if(!loading) {
-            const dates = scheduleList.map(schedule => schedule.dateOption)
-            
-            setDateOptions(dates)
-        }
-    },[scheduleList, loading])
     
     // Handle select date
     function handleSelectedDate(event) {
@@ -39,8 +29,8 @@ export function BookingSchedule() {
             <div className="relative w-11/12 xsm:w-10/12 max-w-[26rem] mx-auto mt-16 mb-9 border-b-4 border-b-primary-dark dark:border-b-white">
                 <select onChange={handleSelectedDate} value={selectedDate} className="w-full pb-3 tracking-[0.3em] text-sm xsm:text-base md:text-lg font-bold uppercase bg-transparent hover:cursor-pointer focus:outline-none appearance-none">
                     <option disabled value="" className="bg-primary-light dark:bg-primary-dark capitalize">Select a Date</option>
-                    { dateOptions.map((date, index) => {
-                        return <option value={date} key={index} className="bg-primary-light dark:bg-primary-dark capitalize">{date}</option>
+                    { scheduleList.map((schedule, index) => {
+                        return <option value={schedule.date} key={index} className="bg-primary-light dark:bg-primary-dark capitalize">{schedule.dateOption}</option>
                     }) }
                 </select>
                 <svg className="absolute top-[0.125rem] right-0 fill-current pointer-events-none" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M16.939 7.939 12 12.879l-4.939-4.94-2.122 2.122L12 17.121l7.061-7.06z"></path></svg>   
